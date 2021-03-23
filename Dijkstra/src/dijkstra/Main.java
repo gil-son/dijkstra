9aebe56571e61f624cb1291828014af4d275be8a
gil-son <gilszon@gmail.com>
2021-03-23 20:08:08 -0300

    Main

--- Dijkstra/src/dijkstra/Main.java
+++ Dijkstra/src/dijkstra/Main.java
@@ -4,13 +4,20 @@ public class Main {
 	public static void main(String []args) {
 		
 		// int A = Integer.MAX_VALUE ,B = Integer.MAX_VALUE,C= Integer.MAX_VALUE,D = Integer.MAX_VALUE,E = Integer.MAX_VALUE;
-		Vertex vertex1 = new Vertex("A",1);
-		Vertex vertex2 = new Vertex("B",2);
-		Vertex vertex3 = new Vertex("C",3);
-		Vertex vertex4 = new Vertex("D",4);
-		Vertex vertex5 = new Vertex("E",5);
 		
 		Dijkstra d = new Dijkstra();
+		
+		
+		
+		Vertex vertex1 = new Vertex("A",1, new String[] {"B","D"}); // Vertex vertex1 = new Vertex("A",1, new String[] {"B","D"});
+		Vertex vertex2 = new Vertex("B",2, new String[] {"A","D","E"});
+		Vertex vertex3 = new Vertex("C",3, new String[] {"B","E"});
+		Vertex vertex4 = new Vertex("D",4, new String[] {"A","B","E"});
+		Vertex vertex5 = new Vertex("E",5, new String[] {"B","C","D"});
+		
+		// Vertex
+		
+		
 		d.newVertex(vertex1);
 		d.newVertex(vertex2);
 		d.newVertex(vertex3);
@@ -18,8 +25,27 @@ public class Main {
 		d.newVertex(vertex5);
 		
 		
+		// Unvisited Vertex
+		d.addUnvisitedVertex(vertex1.getName());
+		d.addUnvisitedVertex(vertex2.getName());
+		d.addUnvisitedVertex(vertex3.getName());
+		d.addUnvisitedVertex(vertex4.getName());
+		d.addUnvisitedVertex(vertex5.getName());
+		System.out.println(d.showUnvisitedVertex());
+		
+		d.removeUnvisitedVertex(vertex1.getName());
+		System.out.println(d.showUnvisitedVertex());
+		
+		// Visited Vertex
+		d.addVisitedVertex(vertex1.getName());
+		//System.out.println(d.showVisitedVertex());
+		
+		System.out.println(d.showVertex());
+		d.searchNeighborVertex(vertex1);
+		
 		// Test before you start
 		
+		/*
 		System.out.println(d.showVertex());
 		
 		d.searchVertex("A");
@@ -33,5 +59,10 @@ public class Main {
 		
 		d.addVertex(new Vertex("Z",12));
 		System.out.println(d.showVertex());
+		*/
+		
+		
+		
+		
 	}
 }
